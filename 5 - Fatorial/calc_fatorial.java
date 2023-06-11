485275d57b8f9a362a49f38bbda91523ee024c10
andremeneses0103 <andremeneses0103@gmail.com>
2023-06-11 16:51:16 -0300

    Programa finalizado: Fatorial

---
+++ 5 - Fatorial/calc_fatorial.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+import javax.swing.text.Style;
+
+public class calc_fatorial{
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int x = Integer.parseInt(input.next());
+        int num = x;
+
+        String calc = Integer.toString(x);
+
+        int y = x -1;
+
+        while(y != 0){
+            calc = calc +" * "+ y;
+            x = x * y;
+            y--;
+            
+        }
+        System.out.println("Fatorial");
+        System.out.println(num + "! = " +calc + " = " + x);
+    }
+}
