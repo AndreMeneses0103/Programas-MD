289e62a961c6020b76715b19bc24fa88b2253f3a
andremeneses0103 <andremeneses0103@gmail.com>
2023-06-12 11:05:13 -0300

    Colocando nome

--- 5 - Fatorial/calc_fatorial.java
+++ 5 - Fatorial/calc_fatorial.java
@@ -5,21 +5,29 @@ public class calc_fatorial{
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Digite um numero: ");
+        System.out.println("Desenvolvido por Andr\u00E9 Meneses // FATEC SJC - Banco de Dados // 1 Semestre // 2023\n");
+        System.out.println("OBS: Valores altos resultarao em numeros negativos ou 0, pois o resultado sera maior que a capacidade da variavel.\n");
+        System.out.print("Digite um numero: ");
         int x = Integer.parseInt(input.next());
         int num = x;
 
-        String calc = Integer.toString(x);
+        if(x == 0){
+            System.out.println(num + "! = 1");
+        }else{
+            String calc = Integer.toString(x);
 
-        int y = x -1;
+            int y = x -1;
+            long resultado = x;
 
-        while(y != 0){
-            calc = calc +" * "+ y;
-            x = x * y;
-            y--;
-            
+            while(y != 0){
+                calc = calc +" * "+ y;
+                resultado = resultado * y;
+                y--;
+                
+            }
+            System.out.println("\nFatorial");
+            System.out.println(num + "! = " +calc + " = " + resultado);
         }
-        System.out.println("Fatorial");
-        System.out.println(num + "! = " +calc + " = " + x);
+
     }
 }
